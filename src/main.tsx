15f0b553bcc2d935f922bb4748f069e345d1939b
Omar Hossa <omarhossa@Omars-MacBook-Air-2.local>
2026-06-10 13:29:38 +0300

    CRITICAL FIX: Wrap App in LanguageProvider and ThemeProvider to fix translation key leaking

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './app/App.tsx';
+import { LanguageProvider } from './app/context/LanguageContext';
+import { ThemeProvider } from './app/context/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
